00c99f3279f93389395a3e9e07e1c9f24b13eb37
dktorres1995 <dktorres95@gmail.com>
2024-11-08 11:08:52 -0500

    Se incluye config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/prueba-tecnica-react/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'src/main.jsx',
+    },
+  },
 })
